afc88c269bec2c0d6c98ff82e32b85624c0b4e38
angxx041 <angxx041@umn.edu>
2016-11-09 13:11:54 -0600

    Solved configure issue

--- config/confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "FlashPhoto"
-#define PACKAGE_TARNAME "flashphoto"
-#define PACKAGE_VERSION "1.0"
-#define PACKAGE_STRING "FlashPhoto 1.0"
-#define PACKAGE_BUGREPORT "harwe006@umn.edu"
-#define PACKAGE_URL ""
