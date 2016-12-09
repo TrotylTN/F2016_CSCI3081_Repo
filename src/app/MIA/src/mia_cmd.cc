c7badf34cd2f6003213d9dd7a8f0b0b414d60260
zhou0745 <zhou0745@umn.edu>
2016-12-09 10:08:17 -0600

    Completed filename parsing for cmd module

--- src/app/MIA/src/mia_cmd.cc
+++ src/app/MIA/src/mia_cmd.cc
@@ -14,7 +14,7 @@
  ******************************************************************************/
 #include "include/mia_cmd.h"
 #include <stdexcept>
-#include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <stack>
 
@@ -46,8 +46,23 @@ MIACmd::MIACmd(void) : filename_({}),
 
 }
 
-MIACmd::MIACmd(int argc, char *argv[]) {
-  MIACmd::MIACmd();
+MIACmd::MIACmd(int argc, char *argv[]) : filename_({}),
+                                         parseresult_(0),
+                                         sharpen_(false),
+                                         sharpen_amount_(0),
+                                         edge_(false),
+                                         threshold_(false),
+                                         threshold_amount_(0),
+                                         quantize_(false),
+                                         quantize_bin_(2),
+                                         blur_(false),
+                                         blur_amount_(0),
+                                         saturate_(false),
+                                         saturate_amount_(0),
+                                         channel_(false),
+                                         channel_red_(0),
+                                         channel_green_(0),
+                                         channel_blue_(0) {
   std::vector <std::string> argstr;
   argstr.clear();
   if (argc == 1) {
@@ -61,16 +76,10 @@ MIACmd::MIACmd(int argc, char *argv[]) {
       this->parseresult_ = HELP_MESSAGE;
       return;
     }
-    if (argstr[i] == "-compare") {
-      if (argstr.size() == 3 && i == 1) {
-        this->parseresult_ = COMPARE_IMG;
-        this->filename_.push_back(make_pair(argstr[0], argstr[2]));
-        return;
-      } else {
-        this->parseresult_ = CMD_ERROR;
-        return;
-      }
-    }
+  }
+
+  if (argstr.size() < 2) {
+    this->parseresult_ = CMD_ERROR;
   }
   std::size_t numofinsign, numofoutsign;
   numofinsign = std::count(argstr[0].begin(), argstr[0].end(), '#');
@@ -81,8 +90,16 @@ MIACmd::MIACmd(int argc, char *argv[]) {
     this->parseresult_ = FILE_ERROR;
     return;
   }
-
   GenFileNamePair(argstr[0], argstr[argstr.size() - 1], numofinsign);
+  if (this->filename_.size() == 0) {
+    this->parseresult_ = FILE_ERROR;
+    return;
+  }
+
+  if (argstr.size() == 3 && argstr[1] == '-compare') {
+    this->parseresult_ = COMPARE_IMG;
+    return;
+  }
 
   for (unsigned long i = 1; i < argstr.size() - 1; i++) {
 
@@ -99,11 +116,20 @@ MIACmd::MIACmd(int argc, char *argv[]) {
  ******************************************************************************/
 
 void MIACmd::GenFileNamePair(std::string infilename,
-                        std::string outfilename,
-                        int rest_sign) {
+                             std::string outfilename,
+                             int rest_sign) {
   if (rest_sign == 0) {
-    this->filename_.push_back(make_pair(infilename, outfilename));
+    std::ifstream filetest(infilename.c_str());
+    if (filetest.good())
+      this->filename_.push_back(make_pair(infilename, outfilename));
     return;
+  }
+  std::size_t pos_sig_in = infilename.find('#');
+  std::size_t pos_sig_out = outfilename.find('#');
+  for (char dig = '0'; dig <= '9'; dig++) {
+    infilename[pos_sig_in] = dig;
+    outfilename[pos_sig_out] = dig;
+    GenFileNamePair(infilename, outfilename, rest_sign - 1);
   }
 }
 
