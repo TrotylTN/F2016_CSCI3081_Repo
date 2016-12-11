fd86d4eb9d8a5b61b80ee5c33dd71ae7fc9d367c
zhou0745 <zhou0745@umn.edu>
2016-12-10 22:39:03 -0600

    completed breif help message for input

--- src/app/MIA/src/main.cc
+++ src/app/MIA/src/main.cc
@@ -34,16 +34,39 @@ int main(int argc, char** argv) {
     image_tools::MIACmd *cmd_parsing;
     cmd_parsing = new image_tools::MIACmd(argc, argv);
     int parsing_state = cmd_parsing->ParseResult();
+    std::string brief_help, full_help;
+    brief_help = "usage: MIA source_file -compare destination_file\n";
+    brief_help += "       MIA source_file ";
+    brief_help += "[-sharpen sharpen_amount(1.0~100.0)]\n";
+    brief_help += "                       [-edge]\n";
+    brief_help += "                       [-threshold threshold_amount(0.0~1.0)]\n";
+    brief_help += "                       [-quantize quantize_bin_number(2~256 integer)]\n";
+    brief_help += "                       [-blur blur_amount(0.0~20.0)]\n";
+    brief_help += "                       [-saturate saturate_amount(-10.0~10.0)]\n";
+    brief_help += "                       [-channel red_amount(0.0~10.0)";
+    brief_help += " green_amount(0.0~10.0) blue_amount(0.0~10.0)]\n";
+    brief_help += "                       filename_to_be_save\n";
+    brief_help += "please enter \"MIA -h\" for more information.\n";
     if (parsing_state == image_tools::MIACmd::FILE_ERROR) {
+      std::cout << "Error: invalid filename or file not existent,"
+                << " please check your input" << '\n';
+      std::cout << brief_help << std::endl;
       return 1;
     }
     if (parsing_state == image_tools::MIACmd::CMD_ERROR) {
+      std::cout << "Error: input cannot be recognized" << '\n';
+      std::cout << brief_help << std::endl;
       return 1;
     }
     if (parsing_state == image_tools::MIACmd::INVALID_FILTER) {
+      std::cout << "Error: input contained invalid filter" << '\n';
+      std::cout << brief_help << std::endl;
       return 1;
     }
     if (parsing_state == image_tools::MIACmd::ARGUMENTS_ERROR) {
+      std::cout << "Error: filters contained invalid argument(s) or "
+                << "didn't have appropriate argument(s)" << '\n';
+      std::cout << brief_help << std::endl;
       return 1;
     }
     if (parsing_state == image_tools::MIACmd::HELP_MESSAGE) {
