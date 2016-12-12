2818eb9046911eb115d4fdf6c7a4867af798ab9a
Zhangjun <xuxx0987@umn.edu>
2016-12-12 00:10:04 -0600

    fix the google style for flter_manager.h

--- src/lib/libimgtools/src/include/filter_manager.h
+++ src/lib/libimgtools/src/include/filter_manager.h
@@ -9,8 +9,8 @@
  *
  ******************************************************************************/
 
- #ifndef SRC_LIB_LIBIMGTOOLS_SRC_INCLUDE_FILTER_MANAGER_H_
- #define SRC_LIB_LIBIMGTOOLS_SRC_INCLUDE_FILTER_MANAGER_H_
+#ifndef SRC_LIB_LIBIMGTOOLS_SRC_INCLUDE_FILTER_MANAGER_H_
+#define SRC_LIB_LIBIMGTOOLS_SRC_INCLUDE_FILTER_MANAGER_H_
 
 /*******************************************************************************
  * Includes
